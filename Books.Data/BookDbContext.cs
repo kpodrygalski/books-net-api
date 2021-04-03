8e3653a514dc48a2e0ed043f15e00951e4a7ce16
Konrad "Rivss" Podrygalski <konrad.podrygalski@gmail.com>
2021-04-03 17:58:24 +0200

    Instaled NuGet packages and created Book model

--- Books.Data/BookDbContext.cs
+++ Books.Data/BookDbContext.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Books.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Books.Data
 {
-    public class BookDbContext
+    public class BookDbContext : DbContext
     {
-        public BookDbContext()
-        {
-        }
+        public BookDbContext() { }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
     }
 }
--- Books.Data/Models/Book.cs
+++ Books.Data/Models/Book.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Books.Data.Models
 {
     public class Book
     {
-
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumbers { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
     }
 }
