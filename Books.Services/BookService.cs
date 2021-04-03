f1515d0a846d48fddb1c52bf0b9f4b1ecc44ea60
Konrad "Rivss" Podrygalski <konrad.podrygalski@gmail.com>
2021-04-03 18:09:52 +0200

    Created and implemented IBookService

--- Books.Services/BookService.cs
+++ Books.Services/BookService.cs
@@ -1,9 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Books.Data;
+using Books.Data.Models;
 using Books.Services.Interfaces;
 
 namespace Books.Services
 {
     public class BookService : IBookService
     {
+        private readonly BookDbContext _db;
+
+        public BookService(BookDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _db.Books.ToList();
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            return _db.Books.Find(bookId);
+        }
+
+        public void CreateBook(Book book)
+        {
+            _db.Books.Add(book);
+            _db.SaveChanges();
+        }
+
+        public void DeleteBookById(int bookId)
+        {
+            var book = _db.Books.Find(bookId);
+
+            if (book != null)
+            {
+                _db.Remove(bookId);
+                _db.SaveChanges();
+            }else
+            {
+                throw new InvalidOperationException("Book not exist!");
+            }
+        }
     }
 }
--- Books.Services/Interfaces/IBookService.cs
+++ Books.Services/Interfaces/IBookService.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using Books.Data.Models;
+
 namespace Books.Services.Interfaces
 {
     public interface IBookService
     {
-
+        public List<Book> GetAllBooks();
+        public Book GetBookById(int bookId);
+        public void CreateBook(Book book);
+        public void DeleteBookById(int bookId);
     }
 }
